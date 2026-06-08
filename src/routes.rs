20e898026b3967a2b94028229e3b4f2caf001b19
benthecarman <benthecarman@live.com>
2026-06-07 20:12:02 -0500

    small cleanup

--- src/routes.rs
+++ src/routes.rs
@@ -90,8 +90,7 @@ pub(crate) async fn get_invoice_impl(
         )
         .await?;
 
-    if invoice.amount_milli_satoshis().is_none()
-        || invoice.amount_milli_satoshis().unwrap() != amount_msats
+    if !invoice.amount_milli_satoshis().is_some_and(|a| a == amount_msats)
     {
         return Err(anyhow!("Invoice amount mismatch"));
     }
